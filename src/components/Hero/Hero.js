0c9e82dc62e51c44e2aa5d66da57d2b43e896b23
mzohaibsigma <m.zohaib@sigmads.com>
2026-09-25 12:47:49 +0500

    image replace

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -33,7 +33,7 @@ const Hero = (props) => (
     </LeftSection>
     <VisualSection>
       <HeroImageWrap>
-        <HeroImage src='/images/profile.jpeg' alt='Engr. Haris Arshad PMP®' />
+        <HeroImage src='/images/haris-arshad-profile.jpeg' alt='Engr. Haris Arshad PMP®' />
       </HeroImageWrap>
     </VisualSection>
   </Section>
